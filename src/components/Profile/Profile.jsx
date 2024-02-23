dbb33fd720650b024453e0297de30a06f4f930de
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-02-23 12:14:24 +0100

    profile done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,20 +13,20 @@ return (
     <p className={css.name}>{username}</p>
     <p class={css.tag}>{tag}</p>
     <p class={css.location}>{location}</p>
-  </div>
 
+  </div>
   <ul className={css.stats}>
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{stats.followers}</span>
+      <span className={css.label}>Followers</span>
+      <span className={css.quant}>{stats.followers}</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">{stats.views}</span>
+      <span className={css.label}>Views</span>
+      <span className={css.quant}>{stats.views}</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{stats.likes}</span>
+      <span className={css.label}>Likes</span>
+      <span className={css.quant}>{stats.likes}</span>
     </li>
   </ul>
 </div>
